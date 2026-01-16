eb75c7fbb0837c8ff141954ba8aea577cb50cfe1
tzmehedy <mehedytouhidurzaman@gmail.com>
2026-01-17 05:47:11 +0600

    The hero section  maked responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,35 @@
 import HeroSection from "@/components/modules/Home/HeroSection";
 import Navbar from "@/components/Shared/Navbar";
+import heroBannerImg1 from "../../public/assets/images/heroimages/hero_banner_img_1.jpg";
 import Image from "next/image";
-import heroBannerImg1 from "../../public/assets/images/heroimages/hero_banner_img_1.jpg"
 
 export default function Home() {
   return (
     <>
       <div className="bg-[#111111] relative overflow-hidden">
+        <div className="w-[45%] h-[45%] rounded-full -rotate-45 bg-linear-to-r from-[#f1b84a] to-yellow-500  absolute top-20 left-0 bottom-40 opacity-60 blur-3xl "></div>
 
-        <div className="w-[45%] h-[45%] rounded-full -rotate-45 bg-linear-to-r from-[#f1b84a] to-yellow-500  absolute top-20 left-0 bottom-40 opacity-60 blur-3xl ">
-          <h1 className="text-white border border-red-500">
-          </h1>
-        </div>
-
-       <div className="absolute top-1/2 min-w-3xl right-0 z-50 ">
-         <div className="relative w-full h-100">
-
-          <Image src={heroBannerImg1} alt="hero1" fill style={{objectFit:"cover"}}/>
-
-        </div>
-       </div>
-
-        <div className="bg-[#1A1A1A] shadow-2xl sticky ">
+        <div className="bg-[#1A1A1A] shadow-2xl fixed top-0 w-dvw z-80">
           <Navbar />
         </div>
+
+        
 
         <div className="container mx-auto">
           <HeroSection />
         </div>
 
-        
-        
-
-        {/* <div className="absolute inset-0 bg-[radial-gradient(circle_at_25%_50%,#facc15_0%,#ca8a04_30%,rgba(0,0,0,0.9)_60%,#000_100%)]"></div> */}
+        <div className="absolute top-2/3  md:w-lg lg:w-3xl right-0 z-50">
+          <div className="relative w-80 md:w-full h-100">
+            <Image
+              src={heroBannerImg1}
+              alt="hero1"
+              quality={100}
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/modules/Home/HeroSection.tsx
+++ src/components/modules/Home/HeroSection.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import circleImage from "../../../../public/assets/images/heroimages/hero_image_circle.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HeroSection() {
   return (
-    <div className="relative p-5 md:p-10  md:border-l md:border-l-muted-foreground md:border-r md:border-r-muted-foreground min-h-screen ">
-      <div className="absolute top-5 md:top-8 right-4 md:right-[35%] animate-spin animation-duration-[10s]">
+    <div className="relative p-5 lg:p-2  md:border-l md:border-l-muted-foreground md:border-r md:border-r-muted-foreground min-h-screen ">
+      <div className="absolute top-5 md:top-20 right-4 md:right-[10%] lg:right-[35%] animate-spin animation-duration-[10s]">
         <Image
           src={circleImage}
           alt="circle"
@@ -19,25 +22,29 @@ export default function HeroSection() {
         />
       </div>
 
-      <div className="text-white mt-12 lg:mt-20 space-y-5 ">
-        <div>
-          <h1 className="text-5xl md:text-7xl lg:text-9xl  font-bold">
-            Building <br />
-            Responsive <br />
-            Websites
-          </h1>
-          <p className="text-justify text-sm text-muted/80 max-w-full md:max-w-1/2">
-            Web design is a critical aspect of creating an effective online
-            presence for any business or individual. A well-designed website not
-            only reflects a brand&apos;s identity but also enhances user
-            experience, ensuring visitors can easily
-          </p>
-        </div>
+      <div className="text-white mt-12 md:mt-20 lg:mt-32 space-y-10 ">
+        <h1 className="text-5xl md:text-7xl lg:text-9xl  font-bold">
+          Building <br />
+          Responsive <br />
+          Websites
+        </h1>
+        <p className="text-justify text-sm text-muted/80 max-w-full md:max-w-1/2">
+          Web design is a critical aspect of creating an effective online
+          presence for any business or individual. A well-designed website not
+          only reflects a brand&apos;s identity but also enhances user
+          experience, ensuring visitors can easily
+        </p>
+
+        <Button
+          variant={"outline"}
+          className=" bg-[#f1b84a] border-none text-foreground/60 font-bold"
+          size={"lg"}
+        >
+          <Link href="/contact-us">Explore Our Projects</Link>
+        </Button>
+
+        <p>5 + M Trusted Clients</p>
       </div>
-
-      
-
-  
     </div>
   );
 }
