b8d2d5ea6a0cd4ce9fb48123bcda90724b84bb28
tzmehedy <mehedytouhidurzaman@gmail.com>
2026-01-21 05:04:30 +0600

    The hero section desing is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import HeroSection from "@/components/modules/Home/HeroSection";
 import Navbar from "@/components/Shared/Navbar";
-import heroBannerImg1 from "../../public/assets/images/heroimages/hero_banner_img_1.jpg";
-import Image from "next/image";
+// import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <div className="bg-[#111111] relative overflow-hidden">
-        <div className="w-[45%] h-[45%] rounded-full -rotate-45 bg-linear-to-r from-[#f1b84a] to-yellow-500  absolute top-20 left-0 bottom-40 opacity-60 blur-3xl "></div>
+      <div className="bg-[#111111] relative ">
+        <div className="overflow-clip w-[45%] h-[45%] rounded-full md:-rotate-45 bg-linear-to-r from-[#f1b84a] to-yellow-500  absolute top-20 left-0 bottom-40 opacity-60 blur-3xl "></div>
 
         <div className="bg-[#1A1A1A] shadow-2xl fixed top-0 w-dvw z-80">
           <Navbar />
@@ -15,20 +14,8 @@ export default function Home() {
 
         
 
-        <div className="container mx-auto">
+        <div className="container mx-auto ">
           <HeroSection />
-        </div>
-
-        <div className="absolute top-2/3  md:w-lg lg:w-3xl right-0 z-50">
-          <div className="relative w-80 md:w-full h-100">
-            <Image
-              src={heroBannerImg1}
-              alt="hero1"
-              quality={100}
-              fill
-              style={{ objectFit: "cover" }}
-            />
-          </div>
         </div>
       </div>
     </>
--- src/components/modules/Home/HeroSection.tsx
+++ src/components/modules/Home/HeroSection.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import circleImage from "../../../../public/assets/images/heroimages/hero_image_circle.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import heroBannerImg2 from "../../../../public/assets/images/heroimages/hero_banner_img_2.jpg";
+import heroBgDotted from "../../../../public/assets/images/heroimages/hero_bg_dotted.jpg";
+import heroBannerImg1 from "../../../../public/assets/images/heroimages/hero_banner_img_1.jpg";
 
 export default function HeroSection() {
   return (
-    <div className="relative p-5 lg:p-2  md:border-l md:border-l-muted-foreground md:border-r md:border-r-muted-foreground min-h-screen ">
-      <div className="absolute top-5 md:top-20 right-4 md:right-[10%] lg:right-[35%] animate-spin animation-duration-[10s]">
+    <div className="relative pb-0 p-5 lg:p-2  md:border-l md:border-l-muted-foreground md:border-r md:border-r-muted-foreground min-h-dvh z-10">
+      <div className="absolute top-10 md:top-20 right-4 md:right-[10%] lg:right-[35%] animate-spin animation-duration-[10s]">
         <Image
           src={circleImage}
           alt="circle"
@@ -22,7 +25,7 @@ export default function HeroSection() {
         />
       </div>
 
-      <div className="text-white mt-12 md:mt-20 lg:mt-32 space-y-10 ">
+      <div className="text-white mt-20  lg:mt-32 space-y-10 ">
         <h1 className="text-5xl md:text-7xl lg:text-9xl  font-bold">
           Building <br />
           Responsive <br />
@@ -43,7 +46,149 @@ export default function HeroSection() {
           <Link href="/contact-us">Explore Our Projects</Link>
         </Button>
 
-        <p>5 + M Trusted Clients</p>
+        <p className="font-bold">5 + M Trusted Clients</p>
+      </div>
+
+      <div className="mt-5 flex">
+        <div className="relative h-16 w-16 rounded-full">
+          <Image
+            className="h-16 w-16 rounded-full border border-white"
+            src={"/assets/images/heroimages/user1.jpg"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className="relative h-16 w-16 rounded-full -ml-3">
+          <Image
+            className="h-16 w-16 rounded-full border border-white"
+            src={"/assets/images/heroimages/user2.jpg"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className="relative h-16 w-16 rounded-full -ml-3">
+          <Image
+            className="h-16 w-16 rounded-full border border-white"
+            src={"/assets/images/heroimages/user3.jpg"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className="relative h-16 w-16 rounded-full -ml-3">
+          <Image
+            className="h-16 w-16 rounded-full border border-white"
+            src={"/assets/images/heroimages/user4.jpg"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className="relative h-16 w-16 rounded-full -ml-3">
+          <Image
+            className="h-16 w-16 rounded-full border border-white"
+            src={"/assets/images/heroimages/user5.jpg"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className="relative h-16 w-16 rounded-full -ml-3">
+          <Image
+            className="h-16 w-16 rounded-full border border-white"
+            src={"/assets/images/heroimages/user6.jpg"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </div>
+
+      <div className="md:absolute  md:top-125 w-full md:w-lg lg:w-3xl md:right-0 z-50 md:-mr-48 mt-2">
+          <div className="relative w-full  md:w-full h-100 ">
+            <Image
+              src={heroBannerImg1}
+              alt="hero1"
+              quality={100}
+              fill
+              style={{ objectFit: "cover" }}
+              className="overflow-hidden"
+            />
+          </div>
+        </div>
+
+
+      <div className="flex justify-end  ">
+        <div className="relative  w-60 h-96 -mr-5 overflow-hidden md:-mr-48 md:-mt-5">
+          <Image
+            className="rotate-x-180 w-60 h-96"
+            src={"/assets/images/heroimages/hero_image_circle_1.png"}
+            alt="user1"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between items-center md:space-x-10 ">
+        <div className="w-full md:w-1/3 md:-ml-48 -mt-20 md:-mt-20 z-80">
+          <div className="relative w-full h-100 md:w-170 md:h-100">
+            <Image
+              src={heroBannerImg2}
+              alt="hero2"
+              quality={100}
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </div>
+
+        <div className="w-dvw md:w-2/3 relative md:-mt-36 md:-mr-48">
+          <div className="bg-yellow-400 w-full h-100 md:h-80  z-60  rounded-xl  ">
+
+            
+            <div className="absolute w-full h-100 md:h-80 opacity-20">
+              <div className="relative md:w-full h-full">
+                <Image
+                  src={heroBgDotted}
+                  alt="dotted"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="rounded-xl"
+                />
+              </div>
+            </div>
+
+            <div className="z-70 flex flex-col md:flex-row justify-center md:justify-between  items-center gap-10 h-full w-full px-10 py-5">
+              <div className="z-80 space-y-5 text-center md:text-start">
+                <h1 className="text-3xl md:text-5xl font-bold">15 k+</h1>
+                <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
+                  Project Complete
+                </p>
+              </div>
+              <div className="z-80 space-y-5 text-center md:text-start">
+                <h1 className="text-3xl md:text-5xl font-bold">10 k+</h1>
+                <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
+                  Happy Customers
+                </p>
+              </div>
+              <div className="z-80 space-y-5 text-center md:text-start">
+                <h1 className="text-3xl md:text-5xl font-bold">03 +</h1>
+                <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
+                  Years Of Experience
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="overflow-hidden lg:overflow-visible  text-white py-2">
+            <div className="whitespace-nowrap animate-marquee  font-bold text-7xl md:text-9xl text-muted-foreground/80 p-2">
+              One Outsourcing BD
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
