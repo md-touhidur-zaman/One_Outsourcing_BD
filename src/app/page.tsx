d576e6c90b91c285aa7bfe3a4856b0c130cdf2b9
tzmehedy <mehedytouhidurzaman@gmail.com>
2026-01-28 03:28:52 +0600

    Hero section is optimize for responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ import Navbar from "@/components/Shared/Navbar";
 export default function HomePage() {
   return (
     <>
-      <div className="bg-[#111111] relative ">
+      <div className=" relative ">
         <div className="overflow-clip w-[45%] h-[45%] rounded-full md:-rotate-45 bg-linear-to-r from-[#f1b84a] to-yellow-500  absolute top-20 left-0 bottom-40 opacity-60 blur-3xl "></div>
 
         <div className="bg-[#1A1A1A] shadow-2xl fixed top-0 w-dvw z-80">
           <Navbar />
         </div>
 
-        <div className="container mx-auto">
+        <div className="bg-[#111111]">
           <HeroSection />
         </div>
       </div>
--- src/components/modules/Home/HeroSection.tsx
+++ src/components/modules/Home/HeroSection.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 import circleImage from "../../../../public/assets/images/heroimages/hero_image_circle.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import heroBannerImg2 from "../../../../public/assets/images/heroimages/hero_banner_img_2.jpg";
 import heroBgDotted from "../../../../public/assets/images/heroimages/hero_bg_dotted.jpg";
 import heroBannerImg1 from "../../../../public/assets/images/heroimages/hero_banner_img_1.jpg";
 
 export default function HeroSection() {
   return (
-    <div className="relative pb-0 p-5 lg:p-2  md:border-l md:border-l-muted-foreground md:border-r md:border-r-muted-foreground min-h-dvh z-10">
-      <div className="absolute top-10 md:top-20 right-4 md:right-[10%] lg:right-[35%] animate-spin animation-duration-[10s]">
+    <div className="relative container mx-auto pb-0 p-5 lg:px-2  md:border-l md:border-l-muted-foreground md:border-r md:border-r-muted-foreground min-h-dvh z-10">
+      <div className="absolute top-10 md:top-20 right-4 md:right-[10%] lg:right-[20%] animate-spin animation-duration-[10s]">
         <Image
           src={circleImage}
           alt="circle"
@@ -106,22 +105,21 @@ export default function HeroSection() {
         </div>
       </div>
 
-      <div className="lg:absolute  lg:top-125 w-full md:w-lg lg:w-3xl md:right-0 z-50 md:-mr-48 mt-2">
-          <div className="relative w-full  md:w-full h-100 ">
-            <Image
-              src={heroBannerImg1}
-              alt="hero1"
-              quality={100}
-              fill
-              style={{ objectFit: "cover" }}
-              className="overflow-hidden"
-            />
-          </div>
+      <div className="flex justify-end w-full mt-5">
+        <div className="relative w-200 h-80">
+          <Image
+            src={heroBannerImg1}
+            alt="hero1"
+            quality={100}
+            fill
+            style={{ objectFit: "cover" }}
+            className=""
+          />
         </div>
+      </div>
 
-
-      <div className="flex justify-end  ">
-        <div className="relative  w-60 h-96 -mr-5 overflow-hidden lg:-mr-48 lg:-mt-5">
+      {/* <div className="flex justify-end  ">
+        <div className="relative  w-60 h-96 -mr-5 overflow-hidden  lg:-mt-5">
           <Image
             className="rotate-x-180 w-60 h-96"
             src={"/assets/images/heroimages/hero_image_circle_1.png"}
@@ -130,64 +128,48 @@ export default function HeroSection() {
             style={{ objectFit: "contain" }}
           />
         </div>
-      </div>
+      </div> */}
 
-      <div className="flex flex-col lg:flex-row justify-between items-center lg:space-x-10 ">
-        <div className="w-full lg:w-1/3 lg:-ml-48 -mt-20 lg:-mt-20 z-80">
-          <div className="relative w-full h-100 lg:w-170 lg:h-100">
-            <Image
-              src={heroBannerImg2}
-              alt="hero2"
-              quality={100}
-              fill
-              style={{ objectFit: "cover" }}
-            />
+      <div className="lg:w-2/3 mt-10 relative z-60">
+        <div className="bg-yellow-400 w-full h-100 lg:h-80  z-60  rounded-t-xl">
+          <div className=" w-full h-100 lg:h-80 opacity-20 right-0">
+            <div className="relative lg:w-full h-full">
+              <Image
+                src={heroBgDotted}
+                alt="dotted"
+                fill
+                style={{ objectFit: "cover" }}
+                className="rounded-xl"
+              />
+            </div>
+          </div>
+
+          <div className="absolute z-70 flex flex-col md:flex-row justify-center md:justify-between  items-center gap-10 h-full w-full px-10 py-5 top-0">
+            <div className="z-80 space-y-5 text-center md:text-start">
+              <h1 className="text-3xl md:text-5xl font-bold">15 k+</h1>
+              <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
+                Project Complete
+              </p>
+            </div>
+            <div className="z-80 space-y-5 text-center md:text-start">
+              <h1 className="text-3xl md:text-5xl font-bold">10 k+</h1>
+              <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
+                Happy Customers
+              </p>
+            </div>
+            <div className="z-80 space-y-5 text-center md:text-start">
+              <h1 className="text-3xl md:text-5xl font-bold">03 +</h1>
+              <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
+                Years Of Experience
+              </p>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div className="w-dvw lg:w-2/3 relative lg:-mt-36 lg:-mr-48">
-          <div className="bg-yellow-400 w-full h-100 lg:h-80  z-60  rounded-xl  ">
-
-            
-            <div className="absolute w-full h-100 lg:h-80 opacity-20">
-              <div className="relative lg:w-full h-full">
-                <Image
-                  src={heroBgDotted}
-                  alt="dotted"
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="rounded-xl"
-                />
-              </div>
-            </div>
-
-            <div className="z-70 flex flex-col md:flex-row justify-center md:justify-between  items-center gap-10 h-full w-full px-10 py-5">
-              <div className="z-80 space-y-5 text-center md:text-start">
-                <h1 className="text-3xl md:text-5xl font-bold">15 k+</h1>
-                <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
-                  Project Complete
-                </p>
-              </div>
-              <div className="z-80 space-y-5 text-center md:text-start">
-                <h1 className="text-3xl md:text-5xl font-bold">10 k+</h1>
-                <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
-                  Happy Customers
-                </p>
-              </div>
-              <div className="z-80 space-y-5 text-center md:text-start">
-                <h1 className="text-3xl md:text-5xl font-bold">03 +</h1>
-                <p className="bg-black text-white font-bold px-3 py-2 rounded-lg">
-                  Years Of Experience
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="overflow-hidden lg:overflow-visible  text-white py-2">
-            <div className="whitespace-nowrap animate-marquee  font-bold text-7xl md:text-9xl text-muted-foreground/80 p-2">
-              One Outsourcing BD
-            </div>
-          </div>
+      <div className="overflow-hidden  text-white py-2 lg:absolute right-0 bottom-0 z-10">
+        <div className="whitespace-nowrap animate-marquee  font-bold text-7xl md:text-9xl text-muted-foreground/80 p-2">
+          One Outsourcing BD
         </div>
       </div>
     </div>
